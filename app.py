dc99922727c07c8a2ec0a8e8cc3852d9b6938e50
dhdhdhdh2002 <eogus353535@naver.com>
2024-11-10 23:19:57 +0900

    app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
 
 
 # 앱 제목
-st.title("나만의 레시피를 소개합니다")
+st.title("맛집 홍보 포스터 제작")
 
 # 재료 입력 받기
-food = st.text_input("어떤 재료를 가지고 계십니까?")
+food = st.text_input("어떤 음식을 홍보하고 싶으신가요?")
 
 # 재료 출력
-if st.button("레시피 생성하기"):
+if st.button("홍보문구 및 포스터 생성"):
      chat_completion = client.chat.completions.create(
         messages=[
             {
@@ -26,6 +26,17 @@ if st.button("레시피 생성하기"):
             }
         ],
         model="gpt-4o",  
+    ) 
+     response = client.images.generate(
+        model="dall-e-3",
+        prompt= f"{food}만화느낌의 포스터로 제작해줘",
+        size="1024x1024",
+        quality="standard",
+        n=1,
 )
-result = chat_completion.choices[0].message.content         
+
+
+result = chat_completion.choices[0].message.content
+image_url = response.data[0].url         
 st.write(result)
+st.image(image_url)
